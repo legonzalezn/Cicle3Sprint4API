eae4f09d80b7e59b322904e55d88a2b160b1a049
legonzalezn <legonzalezn@gmail.com>
2020-12-23 08:38:59 -0500

    Se corrigio la política CORS 2.

--- main.py
+++ main.py
@@ -5,25 +5,10 @@ api = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
-workspace=[
-    "http://localhost.tiangolo.com"
-    , "https://localhost.tiangolo.com"
-    , "http://localhost"
-    , "http://localhost:8080"
-    , "http://127.0.0.1:8080"
-    , "http://127.0.0.1"
-    , "https://reserva-hotel-appvue.herokuapp.com"
-    , "https://cicle3sprint5unal.herokuapp.com"
-]
+workspace=["http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1", "https://reserva-hotel-appvue.herokuapp.com", "https://cicle3sprint5unal.herokuapp.com"]
 
 origins = workspace
 
-api.add_middleware(
-    CORSMiddleware
-    , allow_origins=origins
-    , allow_credentials=True
-    , allow_methods=["*"]
-    , allow_headers=["*"]
-)
+api.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 api.include_router(router_personas)
